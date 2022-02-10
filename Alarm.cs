abf8015f6ad381930eb4e7bffecf8c35d0d9801f
ethulen <73486449+ethulen@users.noreply.github.com>
2022-02-10 09:48:47 -0600

    Add files via upload

---
+++ Alarm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Alarm : INotifyPropertyChanged
+{
+	public Alarm()
+	{
+		
+	}
+	private DateTime _time;
+	public DateTime Time { get { return _time; } set { _time = value; } };
+    private bool _onOrOff;
+
+    public bool OnOrOff
+    {
+        get { return _onOrOff; }
+        set { _onOrOff = value; }
+    }
+
+
+}
